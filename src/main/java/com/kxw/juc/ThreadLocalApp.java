766f1b2d42da3f7c5e6aad0420b5655092e0f355
kangxiongwei <kangxiongwei@meituan.com>
2020-04-18 01:29:17 +0800

    JUC中的同步器

---
+++ src/main/java/com/kxw/juc/ThreadLocalApp.java
@@ -0,0 +1,14 @@
+package com.kxw.juc;
+
+/**
+ * @author kangxiongwei
+ * @date 2020-04-18 01:24
+ */
+public class ThreadLocalApp {
+
+    public static void main(String[] args) {
+        ThreadLocal<String> threadLocal = new ThreadLocal<>();
+
+    }
+
+}
